46a0217c87af40f28761a075db07c5a2ad2c2835
Sonic The <109289447+SonicTheHedgeHog64@users.noreply.github.com>
2024-12-08 02:15:25 -0600

    bobomb buddy 2/2

--- actors/bobomb/geo.inc.c
+++ actors/bobomb/geo.inc.c
@@ -20,7 +20,7 @@ const GeoLayout bobomb_buddy_geo[] = {
 	GEO_OPEN_NODE(),
 		GEO_SHADOW(0, 200, 70),
 		GEO_OPEN_NODE(),
-			GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 4, 0, bobomb_root_mesh_layer_1),
+			GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 4, 0, bobomb_buddy_root_mesh_layer_1),
 			GEO_OPEN_NODE(),
 				GEO_ANIMATED_PART(LAYER_OPAQUE, 15, 33, 0, bobomb_foot_L_mesh_layer_1),
 				GEO_ANIMATED_PART(LAYER_OPAQUE, -15, 33, 0, bobomb_foot_R_mesh_layer_1),
